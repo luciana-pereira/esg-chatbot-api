34ef0e8d695b8748b47ec3b294c9d9b0b2673022
Luciana Pereira <luci.lv14@gmail.com>
2024-03-31 00:23:16 -0300

    config cors

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_restful import Api
+from flask_cors import CORS  # Importe o pacote Flask-CORS
 from routes.chatbot_routes import ChatbotResource
 
 app = Flask(__name__)
@@ -7,6 +8,9 @@ api = Api(app)
 
 # Adicione os recursos da API
 api.add_resource(ChatbotResource, '/chatbot')
+
+# Configuração do CORS
+CORS(app)
 
 if __name__ == '__main__':
     app.run(debug=True)
